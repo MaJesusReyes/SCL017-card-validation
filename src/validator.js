da13158fea4be83fa82d96939342fe9a0fbab701
maJesusReyes <mareyes.esp@gmail.com>
2021-05-19 09:55:26 -0400

    idonknow

--- src/validator.js
+++ src/validator.js
@@ -3,3 +3,49 @@ const validator = {
 };
 
 export default validator;
+
+function obtenerNumero(){
+  let input = document.getElementById("numeroTarjeta").value;
+  let numbers = input.toString().split('').map(Number);
+  let total = 0;
+  let numPar = 0;
+  
+  if (input === ""){
+      alert("Por favor ingresa un número")
+  }
+  else {
+      for (i=0; i<numbers.length; i++){
+          if ( i % 2 === 0) {
+          numPar = numbers[i] * 2
+              if ( numPar > 9 ){
+              numPar = numPar.toString().split('').map(Number);
+              total = total + numPar[0] + numPar[1];
+              }
+              else {
+              total = total + numPar;
+              }
+          }
+          else {
+          total = total + numbers[i];
+          }
+      }
+      console.log(total);
+
+   if (total % 10 === 0){
+      document.getElementById("show").innerText = "V Á L I D O";   
+      }
+   else {
+      document.getElementById("show").innerText = "I N V Á L I D O";    
+      }
+  } 
+}
+
+function mResultado(mostrar) {
+  let result = document.getElementById(mostrar)
+  let input = document.getElementById("numeroTarjeta").value;
+      if(input === ""){
+          result.style.display = "none"; 
+      }
+      else{result.style.display = "block";   
+      }   
+}
